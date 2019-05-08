cadea8c3adecd41e31120cfc0f6f4a9d42f99a80
Pebaz <pebazium@gmail.com>
2019-05-07 21:05:01 -0400

    Created test output files

--- tests.ps1
+++ tests.ps1
@@ -1,4 +1,8 @@
 ls examples | ForEach-Object -Process {
-	Write-Host -ForegroundColor Green "Running ${_}"
-	target/debug/lambda_core -f $_.FullName
+	if ($_.Extension -ne ".output") {
+		Write-Host -ForegroundColor Green "Running ${_}"
+		$file = $_.BaseName
+		$dirr = $_.DirectoryName
+		target/release/lambda_core -f $_.FullName >> "$dirr/$file.output"
+	}
 }
